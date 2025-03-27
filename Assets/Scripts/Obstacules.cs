d10c7e97f3538fda66e0739a89eb1e50804e9b7e
crispeva <crisp8dmo@gmail.com>
2025-03-27 19:12:48 +0100

    Juego terminado definitivamente

--- Assets/Scripts/Obstacules.cs
+++ Assets/Scripts/Obstacules.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Obstacules : MonoBehaviour
 {
     // Start is called before the first frame update
     
     [SerializeField]private GameObject portal;
-    private bool isActive = true;
-    //private bool isActive2 = true;
-    private void Awake()
-    {
-        //portal = GetComponent<GameObject>();
-    }
     void Start()
     {
         InvokeRepeating("TogglePortal", 0f, 2);
--- Assets/Scripts/Victory.cs
+++ Assets/Scripts/Victory.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class Victory : MonoBehaviour
 {
-   [SerializeField] TextMeshPro textMesh;
+   [SerializeField] TextMeshProUGUI textMesh;
     // Start is called before the first frame update
     void Start()
     {
